8224646f572bbd8c11005220201fbbed8c361b52
NaranjoJorge <jpna188@gmail>
2020-02-06 18:56:26 +0000

    created file 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
- * main - prints: with proper grammar, but the outcome is a piece of art,
+ * main - prints
  * Return: 0
  */
+
+int main(void)
+{
+int a = 97;
+int b = 65;
+
+for (; a <= 122; a++)
+{
+putchar(a);
+}
+for (; b <= 90; b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
+}
