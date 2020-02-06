3f079719f2265abc0e433e380861538246eb2aff
NaranjoJorge <jpna188@gmail>
2020-02-06 21:06:39 +0000

    added code

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
- * main - prints: with proper grammar, but the outcome is a piece of art,
+ * main - prints numbers 0 to 9 only using putchar.
  * Return: 0
  */
+
+int main(void)
+{
+	int number = 48;
+
+	while (number < 58)
+
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
