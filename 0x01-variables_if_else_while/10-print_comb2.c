5ddb00b67dbdc6689a10d75fd98a911ce9cccda0
NaranjoJorge <jpna188@gmail>
2020-02-06 23:23:29 +0000

    added code

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - prints: with proper grammar, but the outcome is a piece of art,
+ * main - prints 00, 01, 02, 03, ... 99
  * Return: 0
  */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 57 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
