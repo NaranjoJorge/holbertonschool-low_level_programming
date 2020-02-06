3bd48f13534f1e13966eb2a4a8e3984c2f9501b4
NaranjoJorge <jpna188@gmail>
2020-02-06 21:35:03 +0000

    added code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints: with proper grammar, but the outcome is a piece of art,
+ * main - prints hexadecimals
  * Return: 0
  */
+
+int main(void)
+{
+	int number = 48;
+	int abc = 65;
+
+	while (number < 58)
+
+	{
+		putchar(number);
+		number++;
+	}
+
+	while (abc < 71)
+	{
+		putchar(abc);
+		abc++;
+	}
+
+	putchar('\n');
+	return (0);
+}
