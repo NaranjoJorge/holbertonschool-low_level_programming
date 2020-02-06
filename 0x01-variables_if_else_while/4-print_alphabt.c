fb43e1db178acee47639a006d366cbeb265e89ae
NaranjoJorge <jpna188@gmail>
2020-02-06 20:43:50 +0000

    created file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
- * main - prints: with proper grammar, but the outcome is a piece of art,
+ * main - prints lower case alphabet, excluding q & e
  * Return: 0
  */
+
+int main(void)
+{
+	int abc = 'a';
+
+	while (abc <= 'z')
+
+	{
+		if (abc != 'e' && abc != 'q')
+			
+			putchar(abc);
+		abc++;
+	}
+	putchar('\n');
+	return (0);
+}
