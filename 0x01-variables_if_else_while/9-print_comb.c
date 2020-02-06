dfe903dcc484e28e8d418bb1f8ed4b63247813e0
NaranjoJorge <jpna188@gmail>
2020-02-06 22:28:00 +0000

    added code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - prints: with proper grammar, but the outcome is a piece of art,
+ * main - prints 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
  * Return: 0
  */
+
+int main(void)
+{
+	int numbers = 48;
+
+	while (numbers < 58)
+	{
+		putchar(numbers);
+
+		if (numbers < 57)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		numbers++;
+	}
+	putchar('\n');
+	return (0);
+}
