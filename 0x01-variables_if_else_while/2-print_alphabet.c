01c15ae0195ee0eb6b89491a19914d4a12464d52
NaranjoJorge <jpna188@gmail>
2020-02-06 18:44:02 +0000

    created script 02

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
- * main - prints: with proper grammar, but the outcome is a piece of art,
+ * main - Prints the alphabet.
  * Return: 0
  */
+
+int main(void)
+{
+int a = 97;
+
+for (; a <= 122; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
