f5f7ad260e317ee9e98fca33d0629b7c17fbb0b0
NaranjoJorge <jpna188@gmail>
2020-02-11 12:52:21 +0000

    Created executable that adds two integers.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add -Returns sum of two integers.
+ * @a: integer
+ * @b: integer
+ * Return: a + b.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,14 @@
 #include "holberton.h"
 
-int print_last_digit(int)
-{
+/**
+ * print_last_digit - prints last digit of an integer.
+ * @i: number
+ * Return: 0.
+ */
 
+int print_last_digit(int i)
+{
+	int a = i % 10;
+
+	return (a);
 }
