6e4e8a4ce38e266bf0779ac00dad3f299509c929
NaranjoJorge <jpna188@gmail>
2020-02-13 17:43:34 +0000

    Corrected. Double a++ in for loop.

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,12 +13,11 @@ void print_line(int n)
 	if (n <= 0)
 		_putchar ('\n');
 
-	else if (n > 0)
+	else /*if (n > 0)*/
 	{
 		for  (a = 0; a < n; a++)
 		{
 			_putchar (95);
-			a++;
 		}
 
 		_putchar ('\n');
