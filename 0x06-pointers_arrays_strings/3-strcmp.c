a82d5005c67ec34659a7038ba64b5e867ba4ea66
NaranjoJorge <jpna188@gmail>
2020-02-20 21:59:00 +0000

    Function that compares two strings done.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,22 @@
 #include "holberton.h"
 
 /**
- * _strncpy -Copies/replaces n elements of *src to *dest.
- * @dest: char *dest
- * @src: char *src
- * @n: int n
- * Return: Newly version of dest.
+ * _strcmp - Compares two strings.
+ * @s1: char *s1
+ * @s2: char *s2
+ * Return: Difference.
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	
+	int i;
+
+	for (i = 0; *(s1 + i) != '\0' || *(s2 + i) != '\0'; i++)
+	{
+		if (*(s1 + i) != *(s2 + i))
+		{
+			return (*(s1 + i) - *(s2 + i));
+		}
+	}
+	return (0);
 }
