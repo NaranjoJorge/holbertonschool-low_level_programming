6112d5b2c6d471b8bc23e52d5eca07fb742da5d8
NaranjoJorge <jpna188@gmail>
2020-02-26 18:55:06 +0000

    Added new line at the end.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,13 @@
 void _puts_recursion(char *s)
 {
 
-	if (!*s)
-		return;
-
-	_putchar(*s);
-
-	_puts_recursion(++s);
-
 	if (*s == '\0')
 		_putchar('\n');
+
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+
 }
