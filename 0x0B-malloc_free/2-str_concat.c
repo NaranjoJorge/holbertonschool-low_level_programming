d58759108ed35196307c0ae927a23de67b54b6e5
NaranjoJorge <jpna188@gmail>
2020-03-03 22:46:37 +0000

    Function that concatenates 2 string.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,42 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * _strcat -Concatenates src to dest
- * @dest: char *dest
- * @src: char *src
- * Return: *dest
- */
-
-char *_strcat(char *dest, char *src)
-{
-	int i, a;
-
-	for (i = 0; *(dest + i) != '\0'; i++)
-	{}
-
-	for (a = 0; *(src + a) != '\0'; a++, i++)
-	{
-		*(dest + i) = *(src + a);
-	}
-
-	return (dest);
-}
-
 /**
  * *str_concat -Concatenates 2 strings.
  * @s1: string where s1 & s2 will be concatenate.
  * @s2: string.
  * Return: Pointer to concatenated string.
  */
-
 char *str_concat(char *s1, char *s2)
 {
+	int i, a, c, d;
 	char *s;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
+	{
+		s1 = malloc(1);
+		*s1 = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s2 = malloc(1);
+		*s2 = '\0';
+	}
+	for (i = 0; *(s1 + i) != '\0'; i++)
+	{}
+	for (a = 0; *(s2 + a) != '\0'; a++)
+	{}
+	s = malloc(sizeof(char) * ((i + a) - 1));
+	if (!s)
 		return (NULL);
-
-	/* *s1 */
-	_strcat(s1, s2);
-
-	s = malloc(sizeof(s1));
-
+	for (c = 0; c < i; c++)
+	{
+		*(s + c) = *(s1 + c);
+	}
+	for (d = 0; d < a; d++, c++)
+	{
+		*(s + c) = *(s2 + d);
+	}
+	*(s + c) = '\0';
 	return (s);
 }
