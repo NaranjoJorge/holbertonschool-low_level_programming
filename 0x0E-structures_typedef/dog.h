48f4a909ce40c955d9ef951ffabd457e625c2556
NaranjoJorge <jpna188@gmail>
2020-03-09 21:02:56 +0000

    corrected betty mistake

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,24 +12,20 @@
  *
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 /**
  * my_dog - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *ower);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _putchar(char c);
 
 #endif /*_DOG_H*/
