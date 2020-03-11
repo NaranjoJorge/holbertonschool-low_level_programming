44caca324e686e1cafed372241e827c9348b6331
NaranjoJorge <jpna188@gmail>
2020-03-11 23:24:56 +0000

    Files 3 created but not done.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "function_pointers.h"
+/**
+ * op_add - Returns a + b.
+ * @a:integer
+ * @b:integer
+ * Return: Sum.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns a - b.
+ * @a: integer
+ * @b: integer
+ * Return: difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op-mul -Return a * b.
+ * @a:integer
+ * @b:ineteger
+ * Return: product.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div -Returns a/b.
+ * @a: integer
+ * @b: inetger
+ * Return: quotient.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns a % b.
+ * @a: integer
+ * @b: integer
+ * Return: modulo.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
