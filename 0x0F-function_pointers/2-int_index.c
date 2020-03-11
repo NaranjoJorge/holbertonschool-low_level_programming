c7b8cea6b50606cf47ec4a8ebc23b213a2fe7be0
NaranjoJorge <jpna188@gmail>
2020-03-11 17:41:21 +0000

    File 2. Function that returns 1st element of cmp that does not return 0.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,20 +1,23 @@
 #include "function_pointers.h"
 
 /**
- * int_index - returns the index of the first element for which the cmp function does not return 0.
+ * int_index - returns index of 1st element for which the cmp function does not
+ * return 0.
  * @array: int array.
  * @size: int.
  * @cmp: function pointer.
- * Return: -1 if size < 1. Or the index of the 1st element for which cmp does not return 0.
+ * Return: -1 if size < 1. Or index of the 1st element for which cmp does
+ * not return 0.
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 1)
-		return (-1);
-
-	for (i = 0; 
-	     (*cmp)(i);
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return (-1);
 }
