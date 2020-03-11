fa947b543b7a564deab6fcaaa32bee481cac25a3
NaranjoJorge <jpna188@gmail>
2020-03-11 15:53:57 +0000

    Corrected condition in for loop file 1. Started file 2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 #include <stddef.h>
-
+#include <stdio.h>
 /*
  * Structs, enums and unions definitions
  * Typedefs
@@ -11,5 +11,7 @@
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
 
 #endif /* _FUNCTION_POINTERS_H_ */
