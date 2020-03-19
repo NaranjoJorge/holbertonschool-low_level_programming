80b981be2e633e402985cc46bc566ae5b9c4eb26
NaranjoJorge <jpna188@gmail>
2020-03-19 21:32:36 +0000

    Created proyect

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h:
+ * Return: size_t
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	for(i = 0; h.next[i] != NULL; i++)
+	{
+	}
+	return (i);
+}
