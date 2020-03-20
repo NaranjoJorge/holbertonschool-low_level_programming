abb3291a99c72727cc3e01fe358afafce14f1602
NaranjoJorge <jpna188@gmail>
2020-03-20 17:57:09 +0000

    Did task 3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h:pointer to linked list
+ * Return: size_t
+ */
+
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * list_len - Adds new node at the end of linked list.
+ * @head:Dueble pointer to head
+ * @str:char *
+ * Return: list_t
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int i = 0;
+	list_t *new, *temp;
+
+	new = malloc(sizeof(list_t));
+
+	if (new = NULL)
+		return (NULL);
+
+	new->strdup(str);
+
+	if (str == NULL)
+		return (NULL);
+
+	while(str[i] != '\0')
+		i++;
+
+	new->len = i;
+	new->next = NULL;
+
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next != NULL)
+		temp = temp -> next;
+
+	temp->next = new;
+
+	return (new);
+}
