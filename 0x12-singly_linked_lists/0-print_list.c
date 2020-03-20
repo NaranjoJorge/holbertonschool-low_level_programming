14948d8ad6e818dc6c2d90759db8c2203ee3f562
NaranjoJorge <jpna188@gmail>
2020-03-20 00:38:48 +0000

    Task 0 done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list_t list.
+ * print_list - prints elements of linked list
  * @h:pointer to linked list
  * Return: size_t
  */
@@ -10,17 +10,18 @@ size_t print_list(const list_t *h)
 {
 	unsigned long int i;
 
-	if (!h)
+	while (h != NULL)
 	{
-		printf("[0] (nil)");
-	}
-
-	for(h != NULL)
-	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
 		h = h->next;
-		printf("[%i] %s\n", h->len, h->str);
 		i++;
 	}
-
 	return (i);
 }
