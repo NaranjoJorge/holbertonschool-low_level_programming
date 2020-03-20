dd63c35dcb874521fa17f416d2fad9d85bce90d5
NaranjoJorge <jpna188@gmail>
2020-03-20 01:02:22 +0000

    Created README

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,15 +15,13 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
-			h = h->next;
-			i++;
 		}
 		else
 		{
 			printf("[%i] %s\n", h->len, h->str);
-			h = h->next;
-			i++;
 		}
+		h = h->next;
+		i++;
 	}
 	return (i);
 }
