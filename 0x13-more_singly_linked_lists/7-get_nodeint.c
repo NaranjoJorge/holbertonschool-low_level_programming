00713cc9e211b85d36f7e1696e1daea8c5f87b8e
NaranjoJorge <jpna188@gmail>
2020-03-25 01:19:47 +0000

    Added confition for when index == 0.

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,14 +13,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
+	if (index == 0)
+		return (head);
+
 	while (i < index)
 	{
 		head = head->next;
 		i++;
 	}
 
-	if (head == NULL)
-		return (NULL);
 
 	return (head);
 }
