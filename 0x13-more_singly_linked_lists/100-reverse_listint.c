7c58e7d666fd3a006bf41f1b20fa9ffc1adbce52
NaranjoJorge <jpna188@gmail>
2020-03-25 11:00:54 +0000

    10 with segmentation fault, 100 with error: request for member ‘next’ in something not a structure; in lines 23 & 30.

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,28 +1,39 @@
 #include "lists.h"
 
 /**
- * -------  - frees linked list listint_t.
- * @head:listint_t pointer.
- * Return:void
+ * reverse_listint - Reverses a linked list.
+ * @head: Double pointer to head on linked list listint_t.
+ * Return: Pointer to first node of reversed link.
  */
+
+/*Returns: a pointer to the first node of the reversed list*/
+/*You are not allowed to use malloc, free or arrays*/
 
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev;
 	listint_t *current;
 
-	prev = *head;
-	*head = head->next;
-	current = *head;
+	if (head == NULL)
+		return (0);
 
-	/* disconnecting 1st node from list*/
-	prev->next = NULL;
-	*head = head->next;
-	current->next = prev;
-	/* what am I doing here?*/
-	prev = current;
-	current = *head;
-	
+	if (head != NULL)
+	{
+		*prev = head;
+		head = *head->next;
+		current = *head;
+		/* disconnecting 1st node from list*/
+		prev->next = NULL;
+
+		while (head != NULL)
+		{
+			*head = head->next;
+			current->next = prev;
+			prev = current;
+			current = *head;
+		}
+		*head = prev;
+	}
+	return (prev);
 	/* codeforwin.org/2015/09/c-program-to-reverse-singly-linked-list.html*/
-
 }
