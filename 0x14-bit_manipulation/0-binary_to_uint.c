b635ff27d28aa3d810e403bcd4971d99430a1246
NaranjoJorge <jpna188@gmail>
2020-03-26 13:48:17 +0000

    Created: README,  holberton.h, 0-binary_to_uint.c (need to fix bug, line21. invalid type argument of unary sum += (2 ** i)

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Function that converts binary to decimal.
+ * @b: Pointer to string of 0s & 1s.
+ * Return: Decimal representation of b.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0;
+	unsigned int i;
+
+	if (b == NULL)
+		return (0);
+
+	i = 0;
+	while (*(b + i))
+	{
+		if (*(b + i) == 1)
+			sum += (2 ** i);
+
+		if ((*(b + i) != 1) && (*(b + i) != 0))
+			return (0);
+		i++;
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+Proyect with C code that makes bitwise operations & conversions from different
+bases.
