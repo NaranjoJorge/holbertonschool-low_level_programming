7e3bf980242eaf0536d0689682fe055b9c0099d0
NaranjoJorge <jpna188@gmail>
2020-03-27 21:24:55 +0000

    2-Added condition for n > 64, 3-Wrong output.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,12 +10,16 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	/* unsigned long int counter; */
+
+	if (index > 64)
+		return (-1);
+
 	n >>= index;
 
-		/*
-		 * for (counter = 0; counter == index; counter++)
-		 * n >>= counter;
-		 */
+	/*
+	 * for (counter = 0; counter == index; counter++)
+	 * n >>= counter;
+	 */
 
 	n = n & 1;
 	return (n);
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * set_bit - Sets bit of *n at position index to 1.
+ * @n: unsigned long int.
+ * @index: unsigned int.
+ * Return: 1 if sucess, -1 if failed.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit = 1;
+
+	if (*n > 64)
+		return (-1);
+
+	bit <<= index;
+	*n = (*n | bit);
+
+	return (1);
+}
