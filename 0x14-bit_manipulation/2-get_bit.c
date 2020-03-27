4266664a07a1403e19caabdaa7f6691c46f198d0
NaranjoJorge <jpna188@gmail>
2020-03-27 18:24:02 +0000

    Corrected Betty mistakes. Seems to work.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 
 /**
- * get_bit -
- * @n:
- * @index:
- * Return: 
+ * get_bit - Returns bit in position index.
+ * @n: int.
+ * @index: int.
+ * Return: Bit at position index.
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	/*unsigned long int counter;*/
-
+	/* unsigned long int counter; */
 	n >>= index;
 
-		/*for (counter = 0; counter == index; counter++)
-		n >>= counter;
-		*/
+		/*
+		 * for (counter = 0; counter == index; counter++)
+		 * n >>= counter;
+		 */
 
 	n = n & 1;
 	return (n);
