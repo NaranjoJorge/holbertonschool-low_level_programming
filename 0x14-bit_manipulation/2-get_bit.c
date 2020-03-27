aa9f6efe4e38d54c6bfa8657b9fb29b16b7f457b
NaranjoJorge <jpna188@gmail>
2020-03-27 18:19:42 +0000

    output fine, need to fix betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint - Function that converts binary to decimal.
- * @b: Pointer to string of 0s & 1s.
- * Return: Decimal representation of b.
+ * get_bit -
+ * @n:
+ * @index:
+ * Return: 
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int counter;
+	/*unsigned long int counter;*/
 
-	/* n >>= index */
+	n >>= index;
 
-	for (counter = 0; counter == index; counter++)
+		/*for (counter = 0; counter == index; counter++)
 		n >>= counter;
+		*/
 
 	n = n & 1;
 	return (n);
