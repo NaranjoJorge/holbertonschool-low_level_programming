afc26e12d65ec6018b7e675e008781f2dd12b273
NaranjoJorge <jpna188@gmail>
2020-04-01 04:24:20 +0000

    1/3 of code done.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * create_file - Function that creates a file.
+ * @filename: char *.
+ * @text_content: char *.
+ * Return: 1 on sucess, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+
+	fd = open(filename, O_RDWR);
+
+	if (text_content == NULL)
+		open
+
+	if (fd == -1)
+		return ();
+
+	return (1);
+}
